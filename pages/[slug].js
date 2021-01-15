b9bcf1d306adad5948beeb05779c9c69c80b7681
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2021-01-15 08:53:09 +0530

    Update [slug].js

--- pages/[slug].js
+++ pages/[slug].js
@@ -37,7 +37,7 @@ const Post = ({ postHtml, data }) => {
       <div className="post-container xyz">
         <Link href="/">
           <a>
-            <i className="fa fa-angle-left"></i> Go back
+            Go back
           </a>
         </Link>
         <br />
